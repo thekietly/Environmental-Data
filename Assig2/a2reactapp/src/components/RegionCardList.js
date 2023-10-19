6a083a00ee683af98012bd417ddb581626e7e976
Sammie <lythekiet333@gmail.com>
2023-10-19 15:18:45 +1030

    Add region card list with fetch request
    Add region card list with fetch request

---
+++ Assig2/a2reactapp/src/components/RegionCardList.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+import RegionCard from "./RegionCard"
+const RegionCardList = ({ }) => {
+    const [regionData, setState] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5256/api/A_Regions")
+            .then(response => response.json())
+            .then(data => setState(data))
+            .catch(error => {
+                console.log(error);
+            })
+    })
+
+    return (
+        <div className="row">
+            {regionData.map((obj) => (
+                <RegionCard
+                    key={obj.regionId}
+                    regionId={obj.regionId}
+                    regionName={obj.regionName}
+                    imageUrl={obj.imageUrl}
+                    countryCount={obj.countryCount}
+
+                />
+
+            ))}
+        </div>
+
+    )
+
+}
+
+export default RegionCardList
