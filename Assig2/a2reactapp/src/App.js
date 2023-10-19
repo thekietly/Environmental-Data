4fd799fbb9bdeee7b46c5ab9f87aef056338f856
Sammie <lythekiet333@gmail.com>
2023-10-19 14:34:00 +1030

    Initial commit

--- Assig2/a2reactapp/src/App.js
+++ Assig2/a2reactapp/src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+import { Link, Outlet } from 'react-router-dom';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+
+        <div className="App container">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" href="#">Assignment 2</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <Link className="nav-link active" to="/Home">
+                                Home
+                            </Link>
+                            <Link className="nav-link" to="/Contact">
+                                Regions
+                            </Link>
+                            <Link className="nav-link" to="/Products">
+                                Countries
+                            </Link>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <Outlet />
+
+        </div>
+
+    );
 }
 
 export default App;
