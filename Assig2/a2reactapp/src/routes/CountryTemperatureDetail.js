b6d0a4bed2abe5f79d9a5c25c89502e5b38b8bde
Sammie <lythekiet333@gmail.com>
2023-11-08 20:47:21 +1030

    Add log message to display state data in temperature component
    Add log message to display state data in temperature component

--- Assig2/a2reactapp/src/routes/CountryTemperatureDetail.js
+++ Assig2/a2reactapp/src/routes/CountryTemperatureDetail.js
@@ -1,19 +1,13 @@
-
-const CountryTemperatureDetail = ({ }) => {
-
+import CountryTemperatureTable from '../components/CountryTemperatureTable'
+import { Link, useLocation } from 'react-router-dom';
+const CountryTemperatureDetail = () => {
+    let { state } = useLocation();
+    console.log(state.regionData);
+    console.log(state.countryData);
     return (
-        <div>
-            <h2 className="text-center">
-                CountryTemperatureDetail
-
-            </h2>
-            <hr />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-
-
+        <CountryTemperatureTable />
     )
-
 }
+
 
 export default CountryTemperatureDetail
