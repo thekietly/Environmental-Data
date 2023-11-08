520094c6a89b3fbee3bceda1e01b6c964c62c350
Sammie <lythekiet333@gmail.com>
2023-11-08 18:24:24 +1030

    Add ternary operators to display iso3
    Add ternary operators to display iso3 and cityCount

--- Assig2/a2reactapp/src/components/CountryCard.js
+++ Assig2/a2reactapp/src/components/CountryCard.js
@@ -4,10 +4,8 @@ function Card({ countryId, countryName, iso3, imageUrl, cityCount, emissionDataY
         <div className="card col-4 mb-2" style={{ width: 18 + 'rem' }} >
             <img className="card-img-top" src={imageUrl} alt={"Image of " + countryName} />
             <div className="card-body">
-                <h5 className="card-title">{ iso3 + " : " + countryName}</h5>
-                <p className="card-text">Total cities: {cityCount}</p>
-
-
+                <h5 className="card-title">{iso3 === "" ? "" : iso3 + " : "} {countryName}</h5>
+                <p className="card-text">Total cities: {cityCount === 0 ? "No city records" : cityCount }</p>
                 
                 <p className="card-text">{"Emissions Year Range: " + emissionDataYear[0] + "-" + emissionDataYear[1]} </p>
 
@@ -20,8 +18,6 @@ function Card({ countryId, countryName, iso3, imageUrl, cityCount, emissionDataY
                     temperatureDataYear[0] === 0 ? "" : <Link to={"/Country/CountryTemperatureDetail/" + countryId} className="btn btn-outline-primary">View Temperature</Link>
                 }
                 
-
-
                 <Link to={"/City/" + countryId} className="btn btn-outline-primary">View Cities</Link>
             </div>
 
