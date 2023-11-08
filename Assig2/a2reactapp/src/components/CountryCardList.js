fea7d52cc75758250cfb19fd6b52ee9f7c9dbe6d
Sammie <lythekiet333@gmail.com>
2023-11-08 18:18:03 +1030

    Fix no paramters being passed to country url
    Fix error if no paramters being passed to country url then display all countries

--- Assig2/a2reactapp/src/components/CountryCardList.js
+++ Assig2/a2reactapp/src/components/CountryCardList.js
@@ -4,27 +4,50 @@ import RegionCard from "./RegionCard";
 import { Link, useParams } from 'react-router-dom';
 
 function CountryCardList() {
+    const [query, setQuery] = useState('');
+    
     const { regionId } = useParams();
+    const urlRegionId = regionId || '0';
     const [countryData, setCountryData] = useState({
         theRegion: {},
         countryList: [],
     });
     // fix no regionId being passed over
     useEffect(() => {
-        fetch(`http://localhost:5256/api/B_Countries/CountryList/${regionId}`)
+        fetch(`http://localhost:5256/api/B_Countries/CountryList/${urlRegionId}?searchText=${query}`)
             .then(response => response.json())
             .then(data => {
                 setCountryData(data);
-
             })
             .catch(error => {
-
+                console.log(error);
             });
-    }, [regionId]);
-
-
+    }, [urlRegionId,query]);
+    function handleSubmit(e) {
+        // Prevent the browser from reloading the page
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const formJson = Object.fromEntries(formData.entries());
+        setQuery(formJson.searchTextOnSubmit);
+    }
     return (
-        <div className="row">
+
+        <div>
+            <form method="post" onSubmit={handleSubmit}>
+                <div className="row justify-content-start mb-3">
+                    <div className="col-3">
+                        <input type="text" name="searchTextOnSubmit" className="form-control" placeholder="Handle submit ...Type your query.. " />
+
+                    </div>
+
+
+                    <div className="col text-left">
+                        <button className="btn btn-primary" type="submit">Search</button>
+                    </div>
+                </div>
+
+            </form>
             <div className="card col-4 mb-2" style={{ width: 18 + 'rem' }}>
                 <img className="card-img-top" src={countryData.theRegion.imageUrl} alt={"Image of " + countryData.theRegion.regionName} />
                 <div className="card-body">
@@ -33,20 +56,23 @@ function CountryCardList() {
                     <Link to="/Region" className="btn btn-outline-primary">Back To Regions</Link>
                 </div>
             </div>
-            {countryData.countryList.map((obj) => (
-                <CountryCard
-                //countryId, countryName, iso3, imageUrl, cityCount, emissionDataYear,  temperatureDataYear
-                    key={obj.countryId}
-                    countryId={obj.countryId}
-                    countryName={obj.countryName}
-                    iso3={obj.iso3}
-                    imageUrl={obj.imageUrl}
-                    cityCount={obj.cityCount}
-                    emissionDataYear={obj.emissionDataYearRange}
-                    temperatureDataYear={obj.temperatureDataYearRange}
-                    
-                />
-            ))}
+            <div className="row">
+                {countryData.countryList.map((obj) => (
+                    <CountryCard
+                        //countryId, countryName, iso3, imageUrl, cityCount, emissionDataYear,  temperatureDataYear
+                        key={obj.countryId}
+                        countryId={obj.countryId}
+                        countryName={obj.countryName}
+                        iso3={obj.iso3}
+                        imageUrl={obj.imageUrl}
+                        cityCount={obj.cityCount}
+                        emissionDataYear={obj.emissionDataYearRange}
+                        temperatureDataYear={obj.temperatureDataYearRange}
+
+                    />
+                ))}
+            </div>
+            
         </div>
     );
 }
