d039fb3beadc8ceb47a4a8db815ee8a8a48b1946
Sammie <lythekiet333@gmail.com>
2023-11-13 19:44:59 +1030

    Remove city links if no cities
    Remove city links if no cities

--- Assig2/a2reactapp/src/components/CountryCard.js
+++ Assig2/a2reactapp/src/components/CountryCard.js
@@ -17,7 +17,12 @@ function Card({countryObj, regionObj, countryId, countryName, iso3, imageUrl, ci
                 {
                     temperatureDataYear[0] === 0 ? "" : <Link to={"/Country/CountryTemperatureDetail/" + countryId} state={{ regionData: regionObj, countryData: countryObj }}  className="btn btn-outline-primary">View Temperature</Link>
                 }
-                <Link to={"/City/" + countryId} className="btn btn-outline-primary">View Cities</Link>
+                {
+                    cityCount > 0 && (
+
+                        <Link to={"/City/" + countryId} className="btn btn-outline-primary">View Cities</Link>
+                    )
+                }
             </div>
 
         </div>
