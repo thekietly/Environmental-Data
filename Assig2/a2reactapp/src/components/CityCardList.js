b43a5dc0a0e3ee1c905379959926523a78d0087b
Sammie <lythekiet333@gmail.com>
2023-11-13 19:27:10 +1030

    export CardList and retrieve countryId from url param
    Add export CardList and retrieve countryId from url param

---
+++ Assig2/a2reactapp/src/components/CityCardList.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
+function CityCardList() {
+    const [query, setQuery] = useState('');
+    const { countryId } = useParams();
+
+
+    return (
+
+        <div>
+
+
+
+        </div>
+    );
+}
+
+export default CityCardList;
