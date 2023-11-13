bcc4d7de56bf5f2f9ba2898a8f365704b6ed6790
Sammie <lythekiet333@gmail.com>
2023-11-13 19:32:06 +1030

    Add city fetch request and a search form
    Add city fetch request and a search form
    TODO: city search logic + auto complete city

--- Assig2/a2reactapp/src/components/CityCardList.js
+++ Assig2/a2reactapp/src/components/CityCardList.js
@@ -4,11 +4,41 @@ import { Link, useParams } from 'react-router-dom';
 function CityCardList() {
     const [query, setQuery] = useState('');
     const { countryId } = useParams();
-
-
+    const [cityData, setCityData] = useState([]);
+    useEffect(() => {
+        fetch(`http://localhost:5256/api/C_Cities/${countryId}?searchText=${query}`)
+            .then(response => response.json())
+            .then(data => {
+                setCityData(data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, [countryId]);
+    function handleSubmit(e) {
+        // Prevent the browser from reloading the page
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const formJson = Object.fromEntries(formData.entries());
+        setQuery(formJson.searchTextOnSubmit);
+    }
     return (
 
         <div>
+            <form method="post" onSubmit={handleSubmit}>
+                <div className="row justify-content-start mb-3">
+                    <div className="col-3">
+                        <input type="text" name="searchTextOnSubmit" className="form-control" placeholder="Type a city... " />
+                    </div>
+                    {
+                        /*Auto complete city */
+                    }
+                    <div className="col text-left">
+                        <button className="btn btn-primary" type="submit">Search</button>
+                    </div>
+                </div>
+            </form>
 
 
 
