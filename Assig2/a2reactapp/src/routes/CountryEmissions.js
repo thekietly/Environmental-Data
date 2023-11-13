aeed009ac1ddb1f79008c4628558cf42ebe9d4b2
Sammie <lythekiet333@gmail.com>
2023-11-13 17:41:53 +1030

    Add CountryEmissionDetail component and make use of state object
    Add CountryEmissionDetail component and make use of state object to store region and country details

--- Assig2/a2reactapp/src/routes/CountryEmissions.js
+++ Assig2/a2reactapp/src/routes/CountryEmissions.js
@@ -1,16 +1,12 @@
-const CountryEmissionsData = ({ }) => {
+import CountryEmissionDetail from '../components/CountryEmissionDetail';
+
+import { useLocation } from 'react-router-dom';
+const CountryEmissions = () => {
+    let { state } = useLocation();
     return (
-        <div>
-            <h2 className="text-center">
-                CountryEmissionsData
-            </h2>
-            <hr />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-
-
+        <CountryEmissionDetail regionObj={state.regionData} countryObj={state.countryData} />
     )
 
 }
 
-export default CountryEmissionsData
+export default CountryEmissions
