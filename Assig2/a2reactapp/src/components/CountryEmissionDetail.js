9ec982fde161582197345bebf48572b1150be82c
Sammie <lythekiet333@gmail.com>
2023-11-13 18:15:10 +1030

    Add element fetch request and element select list
    Add element fetch request and element select list
    if select element changes, update the elementId

--- Assig2/a2reactapp/src/components/CountryEmissionDetail.js
+++ Assig2/a2reactapp/src/components/CountryEmissionDetail.js
@@ -4,12 +4,34 @@ import { useState, useEffect } from 'react';
 import CountryEmissionTable from '../components/CountryEmissionTable';
 function CountryEmissionDetail({ regionObj, countryObj }) {
     const { countryId } = useParams();
-    const [emissionData, setEmissionData] = useState({
-        
-    });
-    const [elementData, setElementData] = useState({
+    const [emissionData, setEmissionData] = useState([] 
+    );
+    const [elementData, setElementData] = useState([]);
+    const [elementId, setSelectedElementId] = useState('');
 
-    });
+    useEffect(() => {
+        fetch('http://localhost:5256/api/B_Countries/GetElementList')
+            .then(response => response.json())
+            .then(data => {
+                setElementData(data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+
+    
+
+    useEffect(() => {
+        fetch(`http://localhost:5256/api/B_Countries/CountryEmissionData/${countryId}?${elementId}`)
+            .then(response => response.json())
+            .then(data => {
+                setEmissionData(data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, [countryId]);
     console.log("hello");
     //console.log(countryObj);
     return (
@@ -26,9 +48,18 @@ function CountryEmissionDetail({ regionObj, countryObj }) {
                     <Link to={"/Country/" + regionObj.regionId} className="btn btn-outline-primary">Back to country List</Link>
                 </div>
             </div>
-
+            <select onChange={(e) => setSelectedElementId(e.target.value)}>
+                {elementData.map(element => (
+                    <option key={element.elementId} value={element.elementId}>
+                        {element.elementName}
+                    </option>
+                ))}
+            </select>
             {
                 /*
+                Table
+
+                Summary Table
                 */
             }
 
