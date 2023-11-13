18118f3aedb3b5e3ab28501af2d4f9fec9066e01
Sammie <lythekiet333@gmail.com>
2023-11-13 19:19:45 +1030

    Fetch CountrySummaryEmission
    Fetch CountrySummaryEmission.
    Add table for summary emission if data is not empty

--- Assig2/a2reactapp/src/components/CountryEmissionDetail.js
+++ Assig2/a2reactapp/src/components/CountryEmissionDetail.js
@@ -2,9 +2,12 @@ import { Link, useLocation, useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 import CountryEmissionTable from '../components/CountryEmissionTable';
+import CountrySummaryEmissionTable from '../components/CountrySummaryEmissionTable';
 function CountryEmissionDetail({ regionObj, countryObj }) {
     const { countryId } = useParams();
     const [emissionData, setEmissionData] = useState([] 
+    );
+    const [summaryEmissionData, setSummaryEmissionData] = useState([]
     );
     const [elementData, setElementData] = useState([]);
     const [elementId, setSelectedElementId] = useState('');
@@ -19,7 +22,16 @@ function CountryEmissionDetail({ regionObj, countryObj }) {
                 console.log(error);
             });
     }, []);
-
+    useEffect(() => {
+        fetch(`http://localhost:5256/api/B_Countries/SummaryCountryEmissionData/${countryId}`)
+            .then(response => response.json())
+            .then(data => {
+                setSummaryEmissionData(data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, [countryId]);
     
     const emissionDataAPICall = () => {
         fetch(`http://localhost:5256/api/B_Countries/CountryEmissionData/${countryId}?elementId=${elementId}`)
@@ -36,27 +48,30 @@ function CountryEmissionDetail({ regionObj, countryObj }) {
     //console.log(countryObj);
     return (
 
-        <div>
+        <div className="">
             { /*
                 
             */
             }
-            <div className="card col-4 mb-2">
-                <img src={countryObj.imageUrl} className="card-img-top" alt="country image" />
+            <div className="card col-4 mb-2 mx-auto">
+                <img src={countryObj.imageUrl} className="card-img-top mt-2" alt="country image" />
                 <div className="card-body">
                     <p className="card-text">{countryObj.iso3 === "" ? "" : countryObj.iso3 + ' - '} {countryObj.countryName} {regionObj.regionName === "" ? "" : " from " + regionObj.regionName}</p>
                     <Link to={"/Country/" + regionObj.regionId} className="btn btn-outline-primary">Back to country List</Link>
                 </div>
             </div>
-            <select onChange={(e) => setSelectedElementId(e.target.value)}>
-                <option selected hidden>Select an element..</option>
-                {elementData.map(element => (
-                    <option key={element.elementId} value={element.elementId}>
-                        {element.elementName}
-                    </option>
-                ))}
-            </select>
-            <button onClick={emissionDataAPICall} className="btn btn-outline-primary">Submit</button>
+            <div className="text-left input-group">
+                <select onChange={(e) => setSelectedElementId(e.target.value)} className="form-control col-4">
+                    <option selected hidden>Select an element..</option>
+                    {elementData.map(element => (
+                        <option key={element.elementId} value={element.elementId}>
+                            {element.elementName}
+                        </option>
+                    ))}
+                </select>
+                <button onClick={emissionDataAPICall} className="btn btn-outline-primary">Submit</button>
+            </div>
+            
             {
                 /*
                 Table
@@ -70,7 +85,12 @@ function CountryEmissionDetail({ regionObj, countryObj }) {
                 )
             }
 
-
+            {
+                summaryEmissionData.length > 0 && (
+                    
+                    <CountrySummaryEmissionTable summaryEmissionData={summaryEmissionData} />
+                )
+            }
         </div>
     )
 
