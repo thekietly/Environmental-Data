bebb1afdd37d8e6f39e0d1531ae418e415796ab6
Sammie <lythekiet333@gmail.com>
2023-11-13 20:34:55 +1030

    Fix auto-complete not consistently working
    Add fetch request for auto-complete

--- Assig2/a2reactapp/src/components/CityDetails.js
+++ Assig2/a2reactapp/src/components/CityDetails.js
@@ -6,6 +6,7 @@ function CityDetails() {
     const [query, setQuery] = useState('');
     const { countryId } = useParams();
     const [cityData, setCityData] = useState([]);
+    const [cityAutoComplete, setCityAutoComplete] = useState([]);
     useEffect(() => {
         fetch(`http://localhost:5256/api/C_Cities/${countryId}?searchText=${query}`)
             .then(response => response.json())
@@ -16,34 +17,51 @@ function CityDetails() {
                 console.log(error);
             });
     }, [countryId, query]);
+    useEffect(() => {
+        fetch(`http://localhost:5256/api/C_Cities/${countryId}`)
+            .then(response => response.json())
+            .then(data => {
+                setCityAutoComplete(data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, [countryId]);
     function handleSubmit(e) {
         // Prevent the browser from reloading the page
         e.preventDefault();
         const form = e.target;
         const formData = new FormData(form);
         const formJson = Object.fromEntries(formData.entries());
-        setQuery(formJson.searchTextOnSubmit);
+        setQuery(formJson.citySearchText);
     }
 
     return (
 
         <div>
+
+            { /*
+                        auto-complete approach from the practical test    
+                        */
+            }
             <form method="post" onSubmit={handleSubmit}>
                 <div className="row justify-content-start mb-3">
                     <div className="col-3">
-                        <input list="cities" name="searchTextOnSubmit" className="form-control" placeholder="Type a city... " />
-                        <datalist id="cities">
-                            {cityData.map(city => (
+                        <input list="cityList" name="citySearchText" className="form-control" placeholder="Type a city... " />
+
+                        <datalist id="cityList">
+                            {cityAutoComplete.map(city => (
                                 <option key={city.cityID} value={city.cityName} />
+                                
                             ))}
                         </datalist>
                     </div>
-                    {
-                        /*Auto complete city */
-                    }
+
                     <div className="col text-left">
                         <button className="btn btn-outline-primary" type="submit">Search</button>
                     </div>
+
+
                 </div>
             </form>
 
