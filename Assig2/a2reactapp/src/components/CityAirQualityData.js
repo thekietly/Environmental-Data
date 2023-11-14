afeeef0679568e33fc55420abaaf2cb66fce52ef
Sammie <lythekiet333@gmail.com>
2023-11-14 15:30:25 +1030

    Add city card details
    Fetch air quality data
    Add air quality data card

---
+++ Assig2/a2reactapp/src/components/CityAirQualityData.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+const CityAirQualityData = ({countryId }) => {
+    const { cityId } = useParams();
+
+    const [airQualityData, setAirQualityData] = useState({
+        theCityDetail: {},
+        theCityAirQualityData: [],
+    });
+    useEffect(() => {
+        fetch(`http://localhost:5256/api/C_Cities/GetAirQualityData/${cityId}`)
+            .then(response => response.json())
+            .then(data => {
+                setAirQualityData(data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, [cityId]);
+    return (
+
+        <div>
+            {
+                airQualityData.theCityDetail.cityId === 0 ?
+                    <Link to="/Region" className="btn btn-outline-primary">Back To Regions</Link> : <div className="card col-4 mb-2" style={{ width: 18 + 'rem' }}>
+                        <img className="card-img-top" src={airQualityData.theCityDetail.imageUrl} alt={"Image of " + airQualityData.theCityDetail.regionName} />
+                        <div className="card-body">
+                            <h5 className="card-title">{airQualityData.theCityDetail.cityName}</h5>
+                            <p className="card-text">
+                                {airQualityData.theCityDetail.iso3 === "" ? "" : airQualityData.theCityDetail.iso3 + " : "}
+                                {airQualityData.theCityDetail.countryName}
+                                {airQualityData.theCityDetail.regionName === "" ? "" : " from " + airQualityData.theCityDetail.regionName}</p>
+                            <Link to={"/City/" + countryId} className="btn btn-outline-primary">Back to Cities</Link>
+                        </div>
+                    </div>
+            }
+        </div>
+
+
+    )
+
+}
+
+export default CityAirQualityData
