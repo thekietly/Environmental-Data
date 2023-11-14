8ca15c23ad8009f57a18bdfed7d5882991d1f664
Sammie <lythekiet333@gmail.com>
2023-11-14 15:48:54 +1030

    Create air quality data table component
    Create air quality data table component with temperature table template

---
+++ Assig2/a2reactapp/src/components/CityAirQualityDataTable.js
@@ -0,0 +1,32 @@
+const CityAirQualityDataTable = ({ airQualityData }) => {
+
+    return (
+        <table className="table table-striped table-bordered table-hover table-dark align-middle">
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Temperature Change</th>
+                    <th>Value</th>
+                    <th>Unit</th>
+                    <th>Regional Average</th>
+                    <th>Regional Min</th>
+                    <th>Regional Max</th>
+                </tr>
+            </thead>
+            <tbody>
+                {airQualityData.theCityAirQualityData.map((data, index) => (
+                    <tr key={index}>
+                        <td>{data.theCountryTempData.year}</td>
+                        <td>{data.theCountryTempData.change}</td>
+                        <td>{data.theCountryTempData.value}</td>
+                        <td>{data.theCountryTempData.unit}</td>
+                        <td>{data.regionalAvg}</td>
+                        <td>{data.regionalMin}</td>
+                        <td>{data.regionalMax}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
+export default CityAirQualityDataTable
