bd49a2c36ceae9be3fc5a4abed2ad37ff71f0f8e
SammieDev <lythekiet333@gmail.com>
2025-01-20 14:29:56 +1030

    Remove default css for app.js

--- Assig2/a2reactapp/src/App.js
+++ Assig2/a2reactapp/src/App.js
@@ -8,7 +8,7 @@ function App() {
         <div className="App container">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" href="#">ENV</Link>
+                    <Link className="navbar-brand logo" to="/">CIA</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -17,8 +17,6 @@ function App() {
                             <Link className="nav-link active" to="/Region">
                                 Home
                             </Link>
-
-
                             <Link className="nav-link" to="/Country">
                                 Countries
                             </Link>
