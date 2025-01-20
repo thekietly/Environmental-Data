98c5f81fb928634e016a79b933fc1d9c928301db
SammieDev <lythekiet333@gmail.com>
2025-01-20 14:31:38 +1030

    Remove redundant routes

--- Assig2/a2reactapp/src/index.js
+++ Assig2/a2reactapp/src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Region from './routes/Region';
-import Privacy from './routes/Privacy';
 import Country from './routes/Country';
 import City from './routes/City';
 import Home from './routes/Home';
-import Elements from './routes/Elements';
 import CountryEmissions from './routes/CountryEmissions';
-import SummaryCountryEmissionsData from './routes/SummaryCountryEmissionsData';
 import CountryTemperature from './routes/CountryTemperature';
 import AirQualityData from './routes/AirQualityData';
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -28,11 +24,9 @@ root.render(
                     <Route path="/Country" element={<Country />} />
                     <Route path="/Country/:regionId" element={<Country />} />
                     <Route path="/Country/CountryTemperatureDetail/:countryId" element={<CountryTemperature />} />
-                    <Route path="/Country/SummaryCountryEmissionsData/:countryId" element={<SummaryCountryEmissionsData />} />
+                    {/*<Route path="/Country/SummaryCountryEmissionsData/:countryId" element={<SummaryCountryEmissionsData />} />*/}
 
                     <Route path="/Country/CountryEmissionsDetail/:countryId" element={<CountryEmissions />} />
-                    <Route path="/Country/Elements" element={<Elements />} />
-
                     <Route path="/City/:countryId" element={<City />} />
 
                     <Route path="/City/AirQualityData/:cityId" element={<AirQualityData />} />
@@ -47,8 +41,3 @@ root.render(
         
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
